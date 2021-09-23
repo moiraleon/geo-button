2034767c7038f1fb32a0aefa8721f9147394e6a1
Moira Leon <moira.leon2@gmail.com>
2021-09-22 23:13:28 -0600

    fourth tutorial

--- src/main/java/com/capstone/geolocation/geoController.java
+++ src/main/java/com/capstone/geolocation/geoController.java
@@ -1,44 +1,35 @@
 package com.capstone.geolocation;
-
+//
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//
+//
+//
+//@Controller
+//public class geoController {
+//
+//    @RequestMapping(value = "/")
+//    public String getHome() {
+//        return "home";
+//    }
+//
+//
+//}
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class geoController {
 
-    public geoController() {
+    @GetMapping("/assessment")
+    public String index() {
+        return "index";
     }
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String getHome() {
-        return "home";
-    }
-
-//    @RequestMapping("/")
-//    public String homePage(Model model) {
-//        System.out.println("Home page connected");
-//            return "home.html";
-//    }
-//}
-
-
-//    @RequestMapping("/")
-//    public String homePage(Model model) {
-//        System.out.println("Home page connected");
-//        try {
-//        return "home.html";
-//        } catch
-//        (Exception e) {
-//            System.out.println("Something went wrong.");
-//            return "home";
-//        }
-//    }
-//}
 
 }
